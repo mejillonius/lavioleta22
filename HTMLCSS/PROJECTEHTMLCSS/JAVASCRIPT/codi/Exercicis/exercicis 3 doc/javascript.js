9edb61c87f7fe7b62ffa705f33027f76418a9b2c
mejillonius <liaburns@hotmail.com>
2023-01-17 01:48:13 +0100

    exx1 ex1

---
+++ HTMLCSS/PROJECTEHTMLCSS/JAVASCRIPT/codi/Exercicis/exercicis 3 doc/javascript.js
@@ -0,0 +1,13 @@
+const ex1input = document.querySelector("#ex1 input");
+const ex1button = document.querySelector("#ex1 button");
+const ex1output = document.querySelector("#ex1 p");
+
+
+const ex1 = () => {
+    ex1output.innerHTML = ex1input.value % 2 ? "es impar" : "es par";
+}
+
+
+
+
+ex1button.addEventListener("click",ex1);
