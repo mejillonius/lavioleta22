7c7955b29961981a72cf14f356a5320cad625478
mejillonius <33004749+mejillonius@users.noreply.github.com>
2023-01-19 20:06:24 +0100

    algo

--- HTMLCSS/PROJECTEHTMLCSS/JAVASCRIPT/codi/Exercicis/exercicis 4 doc/exercisis4.js
+++ HTMLCSS/PROJECTEHTMLCSS/JAVASCRIPT/codi/Exercicis/exercicis 4 doc/exercisis4.js
@@ -2,17 +2,40 @@ const ex1BotonMostrar = document.querySelector("#mostrarBotones");
 const ex1BotonBorrar = document.querySelector("#borrarBotones");
 const ex1DivTarget = document.querySelector("#sitioBotones")
 
+
 const ex1Muestra = () => {
-    ex1DivTarget.innerHTML = `  <button type="button" class="btn btn-primary">Primary</button>
-                                <button type="button" class="btn btn-secondary">Secondary</button>
-                                <button type="button" class="btn btn-success">Success</button>
-                                <button type="button" class="btn btn-danger">Danger</button>
-                                <button type="button" class="btn btn-warning">Warning</button>`;
+    ex1DivTarget.innerHTML = `  
+    <a href="https://www.google.es" class="bloquear" >enlace1</a>
+    <a href="https://www.google.es" class="bloquear" >enlace2</a>
+    <a href="https://www.google.es" class="bloquear" >enlace3</a>
+    <a href="https://www.google.es" class="bloquear" >enlace4</a>
+    <a href="https://www.google.es" class="bloquear" >enlace5</a>
+    <a href="https://www.google.es" class="bloquear" >enlace6</a>
+
+    <button type="button" class="desbloquear btn btn-primary">Primary</button>
+     `;
+
+    enlaces = document.querySelectorAll(".bloquear");
+    console.log(enlaces);
+    enlaces.forEach(enlace => {
+        enlace.addEventListener("click",  function(event) {
+            event.preventDefault();
+          });
+    });
+
+    document.querySelector(".desbloquear").addEventListener("click",()=>{
+        enlaces.forEach(enlace => {
+            enlace.removeEventListener("click",preventDefault());
+        });  
+    })
 }
 
 const ex1Borra = () => {
     ex1DivTarget.innerHTML = ``;
 }
 
+
+
 ex1BotonMostrar.addEventListener("click", ex1Muestra);
 ex1BotonBorrar.addEventListener("click",ex1Borra);
+
